82fc9ed2cc627a90ec96970739c5bd0aa52ba7f3
sbravindran03 <ravindran5211@gmail.com>
2024-07-30 12:30:06 +0530

    commit

---
+++ java_if_else_/eg4.java
@@ -0,0 +1,34 @@
+package java_if_else_;
+
+import java.util.Scanner;
+
+// write a java program to check it is duck number or not
+/* 30132 - duck number
+ * 63740 -duck number
+ * 456787654- not a duck number
+ */
+
+public class eg4 {
+    public static void main(String[] args) {
+        Scanner duck = new Scanner(System.in);
+        System.out.println("enter a number :");
+        String duckres = duck.nextLine();
+        boolean ducknumber = false;
+        if (duckres.charAt(0) != '0') { // Check if the number contains '0' but does not start with '0'
+            for (int i = 1; i < duckres.length(); i++) {
+                if (duckres.charAt(i) == '0') {
+                    ducknumber = true;
+
+                }
+                if (ducknumber) {
+                    System.out.println(duckres + "is a duck number");
+
+                } else {
+                    System.out.println(duckres + "is not a duck number ");
+                }
+
+            }
+        }
+
+    }
+}
---
+++ java_if_else_/eg5.java
@@ -0,0 +1,12 @@
+package java_if_else_;
+
+public class eg5 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 4) {
+            System.out.println(a + " ");
+            a++;
+        }
+    }
+}
