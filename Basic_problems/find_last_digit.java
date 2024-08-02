b87dd14692ac961dfb9a54a8032996595400abec
sbravindran03 <ravindran5211@gmail.com>
2024-08-02 20:03:50 +0530

    commit

--- Basic_problems/find_last_digit.java
+++ Basic_problems/find_last_digit.java
@@ -7,9 +7,9 @@ public class find_last_digit {
         Scanner digit = new Scanner(System.in);
         System.out.println("enter a number to digit its last digit ");
         int a = digit.nextInt();
-        String find = Integer.toString(a);
-        int first = find.charAt(0) - '0';
-        int last = find.charAt(find.length() - 1) - '0';
+        String find = Integer.toString(a);// int to string conversion for finding the first and last
+        int first = find.charAt(0) - '0'; // index 0 is used for finding the first number
+        int last = find.charAt(find.length() - 1) - '0'; // (charAt(find.length)-1) -'0' is used to find the last digit
         System.out.println("first number " + first);
         System.out.println("last number " + last);
 
