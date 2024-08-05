37c9522927bf5341172dc6e0d2b301995a79b6c0
sbravindran03 <ravindran5211@gmail.com>
2024-08-05 17:42:59 +0530

    commit

---
+++ Basic_problems/array_min_max.java
@@ -0,0 +1,48 @@
+package Basic_problems;
+
+import java.util.Scanner;
+
+public class array_min_max {
+    static Scanner maxmin = new Scanner(System.in);
+
+    public static int minmax(int[] a, int b) {
+        int mini = Integer.MAX_VALUE;
+        for (int i = 0; i < b; i++) {
+            if (a[i] < mini) {
+                mini = a[i];
+            }
+        }
+        return mini;
+    }
+
+    public static int maximini(int[] a, int b) {
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < b; i++) {
+            if (a[i] > maxi) {
+                maxi = a[i];
+
+            }
+
+        }
+        return maxi;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("enter the size of the array");
+        // inserting the elements
+        int size = maxmin.nextInt();
+        int maximini[] = new int[size];
+        int minmax[] = new int[size];
+
+        for (int i = 0; i < maximini.length; i++) {
+            System.out.println(" enter the value of a[" + i + "]");
+            maximini[i] = maxmin.nextInt();
+            minmax[i] = maxmin.nextInt();
+            System.out.println("maxmimum: " + maximini(maximini, size));
+            System.out.println("minimum: " + minmax(minmax, size));
+
+        }
+    }
+
+}
---
+++ Basic_problems/pallindrone.java
@@ -0,0 +1,21 @@
+package Basic_problems;
+
+import java.util.Scanner;
+
+public class pallindrone {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter number to check");
+        String result = num.next();
+        int length = result.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (result.charAt(i) != result.charAt(length - i - 1)) {
+                System.out.println("its not a pallindrone");
+            } else {
+                System.out.println("it is a pallindrone");
+            }
+
+        }
+    }
+
+}
