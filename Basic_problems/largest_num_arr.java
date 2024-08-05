62a5add01cb75726de3ec263559eb6bdb223d91a
sbravindran03 <ravindran5211@gmail.com>
2024-08-05 18:39:19 +0530

    commit

---
+++ Basic_problems/largest_num_arr.java
@@ -0,0 +1,31 @@
+package Basic_problems;
+
+import java.util.Scanner;
+
+public class largest_num_arr {
+    public static void main(String[] args) {
+        Scanner lar = new Scanner(System.in);
+        System.out.println(" enter size of the array");
+        int large = lar.nextInt();
+        int largest[] = new int[large];
+        // loop inserting the elements
+        for (int i = 0; i < largest.length; i++) {
+            System.out.println("enter the number of a[" + i + "]");
+            largest[i] = lar.nextInt();
+
+        }
+
+        // loop for finding the largest
+        int n = largest[0];
+        for (int i = 0; i < largest.length; i++) {
+            if (largest[i] > n) {
+                n = largest[i];
+
+            }
+
+        }
+        System.out.println("largest number in the array is: " + n);
+
+    }
+
+}
