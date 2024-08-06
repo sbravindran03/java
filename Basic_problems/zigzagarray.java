904b6302f78c8184f9e0b90f965fd19a4cae27ed
sbravindran03 <ravindran5211@gmail.com>
2024-08-06 09:33:08 +0530

    commit

--- Basic_problems/zigzagarray.java
+++ Basic_problems/zigzagarray.java
@@ -6,6 +6,7 @@ public class zigzagarray {
     public static void main(String[] args) {
         int[] a = { 10, 20, 30, 40 };
         int[] b = { 50, 60, 70, 80 };
+        // loop for ZIGZAG ARRAY
         int[] res = new int[a.length + b.length];
         for (int i = 0, m = 0, n = b.length - 1; i < res.length; i++) {
             if (i % 2 == 0)
@@ -14,9 +15,9 @@ public class zigzagarray {
                 res[i] = b[n--];
 
         }
-        System.out.println("a" + Arrays.toString(a));
-        System.out.println("b" + Arrays.toString(b));
-        System.out.println("c" + Arrays.toString(res));
+        System.out.println("a =" + Arrays.toString(a));
+        System.out.println("b =" + Arrays.toString(b));
+        System.out.println("c =" + Arrays.toString(res));
     }
 
 }
