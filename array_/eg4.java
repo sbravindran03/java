373498732980df6840bf952497d5e0f9deb0b9d2
sbravindran03 <ravindran5211@gmail.com>
2024-08-07 16:30:22 +0530

    commit

--- array_/eg4.java
+++ array_/eg4.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class eg4 {
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
-        System.out.println("enter the size of the array");
+        System.out.println("enter the sizre of the array");
         int size = user.nextInt();
         int a[] = new int[size];
         // loop for inserting new elements
@@ -16,7 +16,7 @@ public class eg4 {
         }
         // loop for printing
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+
         }
 
     }
--- array_/leftzero.java
+++ array_/leftzero.java
@@ -1,5 +1,26 @@
 package array_;
 
+import java.util.Arrays;
+
 public class leftzero {
+    public static void main(String[] args) {
+        int[] a = { 1, 3, 3, 4, 0, 3, 4, 6, 0, 1 };
+        int left = a.length - 1, right = 0;
+        System.out.println(Arrays.toString(a));
+
+        while (left > right) {
+            if (a[left] != 0 && a[right] == 0) {
+                int temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+
+            }
+            if (a[left] != 0)
+                left--;
+            if (a[right] == 0)
+                right++;
+        }
+        System.out.println(Arrays.toString(a));
+    }
 
 }
