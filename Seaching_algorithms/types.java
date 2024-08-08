8b2f83de0e1ed8e74e4021a2b77b3d2e662f963a
sbravindran03 <ravindran5211@gmail.com>
2024-08-08 08:40:11 +0530

    commit

---
+++ Seaching_algorithms/types.java
@@ -0,0 +1,22 @@
+package Seaching_algorithms;
+//LINEAR SEARCH
+
+//BINARY SEARCH
+//DFS AND BFS
+//JUMP SEARCH
+//INTER POLATION SEARCH
+//TERNARY SEARCH
+//EXPONENTIAL SEARCH
+
+public class types {
+    /* sorting algorithms */
+    // BUBBLE SORT
+    // SELECTION SORT
+    // MERGE SORT
+    // QUICK SORT
+    // INSERTION SORT
+    // RADIX SORT
+    // counting sort
+    // parralel sort
+    // bucket sort
+}
---
+++ array_/equality.java
@@ -0,0 +1,17 @@
+package array_;
+
+import java.util.Scanner;
+
+public class equality {
+    public static void main(String[] args) {
+        Scanner equal = new Scanner(System.in);
+        System.out.println("enter the size of the array A");
+        int sizea = equal.nextInt();
+        int[] a = new int[sizea];
+        System.out.println("enter the size of the array B");
+        int sizeb = equal.nextInt();
+        int[] b = new int[sizeb];
+
+    }
+
+}
