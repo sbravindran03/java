f4c3393a83533aec7d13207da8499719534374c4
sbravindran03 <ravindran5211@gmail.com>
2024-08-09 08:51:46 +0530

    commit

---
+++ Basic_problems/upper_to_lower.java
@@ -0,0 +1,19 @@
+package Basic_problems;
+
+import java.util.Arrays;
+
+public class upper_to_lower {
+    public static void main(String[] args) {
+        char[] ch = { 'A', 'b', 'c', 'k', 'm', '$', '#' };
+        System.out.println(Arrays.toString(ch));
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'A' && ch[i] <= 'Z')
+                ch[i] += 32;// for changing upper case to lower case is +32
+            else if (ch[i] >= 'a' && ch[i] <= 'z')
+                ch[i] -= 32;// for changing lower case to upper is -32
+
+        }
+        System.out.println(Arrays.toString(ch));
+    }
+
+}
---
+++ Searching_algorithms/bubble_sort_eg1.java
@@ -0,0 +1,5 @@
+package Searching_algorithms;
+
+public class bubble_sort_eg1 {
+
+}
