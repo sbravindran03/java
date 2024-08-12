da88be641193f900b4b7265abf425b2153dd6d16
sbravindran03 <ravindran5211@gmail.com>
2024-08-12 08:32:05 +0530

    commit

--- Basic_problems/calculator.java
+++ Basic_problems/calculator.java
@@ -6,25 +6,26 @@ public class calculator {
     public static void main(String[] args) {
         System.out.println("enter the arithmatic operation");
         Scanner calcu = new Scanner(System.in);
-        System.out.println("1 for additon 2 for sub , 3 for multipliction,4 for divison ");
-        int res = calcu.nextInt();
+
+        String res = calcu.next();
         System.out.println("enter the nmber a");
         int a = calcu.nextInt();
         System.out.println("b");
         int b = calcu.nextInt();
-        if (res == 1) {
-            res = a + b;
+        int result = 0;
+        if (res.equals("+")) {
+            result = a + b;
 
-        } else if (res == 2) {
-            res = a - b;
+        } else if (res.equals("-")) {
+            result = a - b;
 
-        } else if (res == 3) {
-            res = a * b;
+        } else if (res.equals("*")) {
+            result = a * b;
 
-        } else if (res == 4) {
-            res = a / b;
+        } else if (res.equals("/")) {
+            result = a / b;
         }
-        System.out.println(res);
+        System.out.println(result);
     }
 
 }
