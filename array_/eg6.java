2ddf03e348886c4237fed82fa715636bacd5912f
sbravindran03 <ravindran5211@gmail.com>
2024-08-17 18:06:01 +0530

    commit

--- array_/eg6.java
+++ array_/eg6.java
@@ -1,5 +1,24 @@
 package array_;
 
-public class eg6 {
+import java.util.Scanner;
 
+public class eg6 {
+    // array for inserting the elememt
+    public static void main(String[] args) {
+        System.out.println("enter the size of the array");
+        Scanner krishav = new Scanner(System.in);
+        int size = krishav.nextInt();
+        int[] a = new int[size];
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("enter the elements of a[" + i + "]");
+            a[i] = krishav.nextInt();
+
+        }
+        // for printing the elements in the array
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + "");
+        }
+
+    }
 }
