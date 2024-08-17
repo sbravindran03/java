847fc868da71345b7b19acc91cd172a05eddab34
sbravindran03 <ravindran5211@gmail.com>
2024-08-17 17:56:24 +0530

    commit

--- array_/traverse.java
+++ array_/traverse.java
@@ -11,6 +11,8 @@ public class traverse {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] + a[j] == target)
                     System.out.println("[" + a[i] + "," + a[j] + "]...");
+                // traverse
+
             }
         }
     }
