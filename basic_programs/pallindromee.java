9e317fb6ab77cbf172d70631d1b584818ae563ac
sbravindran03 <ravindran5211@gmail.com>
2024-08-23 19:28:36 +0530

    commit

---
+++ basic_programs/pallindromee.java
@@ -0,0 +1,17 @@
+package basic_programs;
+
+import java.util.Scanner;
+
+public class pallindromee {
+
+    public static void main(String[] args) {
+        int r, sum = 0, temp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+        temp = n;
+        while (n > 0) {
+            r = n % 10;
+        }
+    }
+}
