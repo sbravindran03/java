3ad2f4e45e14f569486d9e8f568288211a1444aa
sbravindran03 <ravindran5211@gmail.com>
2024-08-24 09:44:16 +0530

    commit

--- oops/nonstatic/jdk.java
+++ oops/nonstatic/jdk.java
@@ -1,6 +1,7 @@
 package oops.nonstatic;
 
 public class jdk {
+
     //jdk architecture
     /*when ever a programmer exceute the program at the run time following meomry blocks
     will be created 
@@ -14,8 +15,28 @@ public class jdk {
      * step3 ->in table will be created inside the class block 
      * step 4->all the static members present in the method area will be loaded inside the table 
      * step5 -> all the static varaibeles present inside the class will be loaded with default values inside the class block
-     * 
+     * step 6-> all the static initilizers both single line and multline exceute from top -bottom approach
+     * step 7-> once all the static initlizers execution completed jvm will calll main method and excution execution ends at main method
      * 
      */
+    static int a = 10;
+    int b = 10;
 
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("bye");
+
+    }
+
+    static {
+        System.out.println("MLSI");
+    }
+
+    {
+        System.out.println("MLNSI");
+    }
+
+    public void m1() {
+        System.out.println("i am non static method");
+    }
 }
