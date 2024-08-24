09c3c00384a85fe659e46a35d337a33aad8d3d82
sbravindran03 <ravindran5211@gmail.com>
2024-08-24 09:01:47 +0530

    commit

--- oops/nonstatic/defintion.java
+++ oops/nonstatic/defintion.java
@@ -17,6 +17,35 @@ public class defintion {
     String name;//---non static varible declaration
     double b;//---non static varible declaration
     //what is non static methods
-    /*any method which is not prefix with static  is called as non static  */
 
+    /*any method which is not prefix with static  is called as non static  */
+    //the are present inside the non static block called as non static context (they are having local area )
+    public void run() {
+        //non-static context or local area
+        System.out.println("hi");
+    }
+
+    public int stop() {
+        int b = 20;// local variable
+        return 23;
+    }
+
+    //what is singleline non static initilizer 
+    /*any non static varible initilized in a single line  non static intilizer
+    int d =10;SLNSI---
+    int b;--non static variable declaration statement;
+     * what is multi line non static initilizer?
+     * a block which is block not prefix with anything is called as multiline initilizer
+     */
+    {
+    }//---multiline non static inilizer or  anonymous block
+
+    {
+    }//---multiline non static inilizer or  anonymous block
+
+    {
+    }
+
+    {
+    }//---multiline non static inilizer or  anonymous block
 }
