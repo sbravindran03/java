7e2c6f8e6eb1b1c8cfa8c53c6aa477a85e86628a
sbravindran03 <ravindran5211@gmail.com>
2024-08-24 08:48:50 +0530

    commit

---
+++ oops/nonstatic/defintion.java
@@ -0,0 +1,22 @@
+package oops.nonstatic;
+//with respect 3 
+/*non static varaibles 
+ * non static methods
+ * non static initializers
+ * single line non static inilizer
+ * multi line non static intilizer
+ what is non static variable?
+ the variable which is declared or declared and initialized  in the global area and prifix with called as non stattic variable
+ we cant say local variables are non static varaibles
+ non static varaibles are the member of the global area 
+ 
+ */
+public class defintion {
+
+    int a;//---non static varible declaration
+    String name;//---non static varible declaration
+    double b;//---non static varible declaration
+    //what is non static methods
+    /*any method which is not prefix with static  is called as non static  */
+
+}
